94c7bd1dacc50838d7fa82c377ef14ba4745d104
tiarroelpridatamba1108 <tiarrotamba24@gmail.com>
2022-06-05 19:29:37 +0700

    challenge-callback task

--- index.js
+++ index.js
@@ -6,15 +6,28 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (names, sortFunc) => {
+  const sortNames = sortFunc(names);
+  const result = [];
+
+  for (let i = 0; i < sortNames.length; i++) {
+    result.push(`Urutan List Nama : ${i+1}. ${sortNames[i]}`);
+  }
+
+  return result;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (names) => {
+  return names.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (names) => {
+  return names.reverse();
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
